7610df878133220135c9c8017fa60e41ebdbd25f
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-11 23:37:41 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,6 +1,3 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
-
 from pyrogram import filters
 from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
@@ -10,4 +7,12 @@ from WebStreamer.bot import StreamBot
 async def start(_, m: Message):
     await m.reply(
         f'Hi {m.from_user.mention(style="md")}, ✅ Send me a ⏺ file to get ❎ an instant stream link.'
+    )
+
+@StreamBot.on_message(filters.private & filters.command(["about"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=ABOUT_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=ABOUT_BUTTONS
     )

be162cee306d9717b0a7b77f9ecb9991d6e7e095
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 23:31:16 +0530

    Update commands.py

--- WebStreamer/bot/plugins/commands.py
+++ WebStreamer/bot/plugins/commands.py
@@ -105,3 +105,101 @@ All tutorials related to Bots, Website, Movies and etc, will be updated here. Ti
     ),
         disable_web_page_preview=True,
     )
+    
+@StreamBot.on_message(filters.command('about'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>😊 About</b>
+        
+<b>✯ My Name:</b> Hagadmansa Mega Bot
+<b>✯ Creator:</b> <a href='https://t.me/hagadmansa'>Hagadmansa</a>
+<b>✯ Library:</b> <a href='https://pyrogram.org'>Pyrogram</a>
+<b>✯ Language:</b> <a href='https://Python.org'>Python</a>
+<b>✯ Database:</b> <a href='https://mongodb.com'>MongoDB</a>
+<b>✯ Server:</b> <a href='https://heroku.com'>Heroku</a>
+<b>✯ Channel:</b> <a href='https://t.me/hagadmansa'>Hagadmansa</a>
+<b>✯ Group:</b> <a href='https://t.me/hagadmansachat'>Hagadmansa Support</a>
+<b>✯ Brothers:</b> <a href='https://t.me/hagadmansabot'>Hagadmansa Bot</a>, <a href='https://t.me/hagadmansarobot'>Hagadmansa Robot</a>""",
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🌐 Visit Our Website', url='https://hagadmansa.com')
+            ],[
+            InlineKeyboardButton('⭐️ Rating', callback_data='rating'),
+            InlineKeyboardButton('❤️ Source', callback_data='source'),
+            ],[
+            InlineKeyboardButton('🔙 Back', callback_data='home'),
+            InlineKeyboardButton('🤝 Donate', callback_data='donate')
+        ]]
+    ),
+        disable_web_page_preview=True,
+    )
+    
+@StreamBot.on_message(filters.command('rating'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>😊 About</b> > Rating
+        
+I have a public channel a private channel and 3 bots, along with this also I have a website. you can rate and write a review on our public channel and bots.
+
+<b>📣 @hagadmansa</b>
+
+1. Details are <a href='https://t.me/tlgrmcbot?start=hagadmansa'>here</a>
+2. Review this channel <a href='https://t.me/tlgrmcbot?start=hagadmansa-review'>here</a>
+
+<b>🤖 @hagadmansabot</b>
+
+1. Details are <a href='https://t.me/tlgrmcbot?start=hagadmansabot'>here</a>
+2. Review this bot <a href='https://t.me/tlgrmcbot?start=hagadmansabot-review'>here</a>
+
+<b>🤖 @hagadmansarobot</b>
+
+1. Details are <a href='https://t.me/tlgrmcbot?start=hagadmansarobot'>here</a>
+2. Review this bot <a href='https://t.me/tlgrmcbot?start=hagadmansarobot-review'>here</a>
+
+<b>🤖 @hagadmansamegabot</b> 
+
+1. Details are <a href='https://t.me/tlgrmcbot?star=hagadmansamegabot'>here</a>
+2. Review this bot <a href='https://t.me/tlgrmcbot?start=hagadmansamegabot-review'>here</a>""",
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+    ),
+        disable_web_page_preview=True,
+    )
+
+@StreamBot.on_message(filters.command('source'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>😊 About</b> > Source
+        
+<b>❗️NOTE:</b>
+
+We are not open source yet, if in future we are open our code for everyone then we'll update the source code here."""
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+    ),
+        disable_web_page_preview=True,
+    )
+    
+@StreamBot.on_message(filters.command('donate'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>😊 About</b> > Donate
+        
+<b>❗️NOTE:</b>
+
+We are not raising any funds right now, if in future we raise funds then we'll update here."""
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+    ),
+        disable_web_page_preview=True,
+    )
+    

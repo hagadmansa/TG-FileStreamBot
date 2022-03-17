5f8bcbd0be039f0d74f8da35bbc8d12c8f429c75
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 20:02:50 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -2,12 +2,6 @@ from pyrogram import filters
 from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-    
-@StreamBot.on_message(filters.command(["about"]))
-async def start(_, m: Message):
-    await m.reply(
-        f'follow me on telegram <a href="https://hagadmansa.com/telegram/">here</a>.'
-    )
    
 @StreamBot.on_message(filters.command('start'))
 async def command(b, m:Message):

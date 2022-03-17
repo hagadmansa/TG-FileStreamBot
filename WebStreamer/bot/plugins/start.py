3b49dfe6cddf092ba454ba0b583cc0e5cdb35d76
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 17:49:20 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -38,10 +38,37 @@ START_TEXT = """
 🧐 Don't know how to do? No worries, just press the help button.
 
 👨‍💻 My Creator is <a href=https://t.me/hagadmansa>Hagadmansa</a>."""
-
     
 HELP_TEXT = """<b>ℹ️ HELP</b>
 Here is the list of my commands, please read carefully everything. if anything happened to you then we are not responsible."""
+
+HOWTOUSEME_TEXT = """<b>ℹ️ Help</b> > How To Use Me
+<b>👤 For an individual:</b>
+My name is Hagdmansa Mega Bot, I am a member of Hagdmansa family. I can provide you direct download link of any telegram file/media. If you send me any file/media I will give an external download link, you can use that link to download any file outside telegram. My link is supported in any browser.
+• Send me any file/media from Telegram.
+• I Will provide an external download link for you.
+• All links will be permanent and have the fastest speed.
+<b>👥 For groups/channels:</b>
+I also work in public/private groups/channels. If you have multiple files in your group/channel then just add me to your group/channel, I will add an external download link on each file which will be added after I joined the group/channel.
+• I must be an admin in your channel/group.
+• Don't forget to give all permissions otherwise I will not work.
+<b>🔞 Warning:</b>
+• 18+ content and pornography are strictly prohibited. Don't send me any pornographic/violent videos. You will get an instant ban if we see any kind of content like this."""
+
+INSTRUCTIONS_TEXT = """<b>ℹ️ Help</b> > Instructions
+<b>👤 Instructions for an individual:</b>
+1. Don't send photos to the bot, send them as a file.
+2. Don't send multiple files at a time, send them one by one.
+<b>👥 Instructions for groups/channels:</b>
+1. Don't send too many files to your groups/channels.
+2. Bot takes time to generate and edit links, keep patience.
+<b>🔞 Warning:</b>
+• 18+ content and pornography are strictly prohibited. Don't send me any pornographic/violent videos. You will get an instant ban if we see any kind of content like this."""
+
+TUTORIALS_TEXT = """<b>ℹ️ Help</b> > Tutorials
+All tutorials related to Bots, Website, Movies and etc, will be updated here. Till then you can visit my movie website <b>www.hagadmansa.com</b> to watch movies. Don't forget to subscribe my updates channel <b>@hagadmansa</b>.
+<b>🔞 Warning:</b>
+• 18+ content and pornography are strictly prohibited. Don't send me any pornographic/violent videos. You will get an instant ban if we see any kind of content like this."""
 
 @StreamBot.on_callback_query()
 async def cb_data(bot, update):
@@ -56,7 +83,25 @@ async def cb_data(bot, update):
             text=HELP_TEXT,
             disable_web_page_preview=True,
             reply_markup=HELP_BUTTONS
-         )      
+         )
+    elif update.data == "howtouseme":
+       await update.message.edit_text(
+            text=HOWTOUSEME_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HOWTOUSEME_BUTTONS
+        )
+    elif update.data == "instructions":
+        await update.message.edit_text(
+            text=INSTRUCTIONS_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=INSTRUCTIONS_BUTTONS
+        )
+    elif update.data == "tutorials":
+        await update.message.edit_text(
+            text=TUTORIALS_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=TUTORIALS_BUTTONS
+        )
         
 START_BUTTONS = InlineKeyboardMarkup(
         [[
@@ -78,4 +123,22 @@ HELP_BUTTONS = InlineKeyboardMarkup(
             InlineKeyboardButton('🔙 Back', callback_data='home'),
             InlineKeyboardButton('📣 Channel', url='https://t.me/hagadmansa')
         ]]
+    )
+HOWTOUSEME_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+    )
+INSTRUCTIONS_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+   )
+TUTORIALS_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
     )

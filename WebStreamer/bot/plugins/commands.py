bd4aa4f3cd55bcf51dccc8e928c413cda187c207
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 23:10:53 +0530

    Update commands.py

--- WebStreamer/bot/plugins/commands.py
+++ WebStreamer/bot/plugins/commands.py
@@ -23,7 +23,6 @@ async def command(b, m:Message):
         ]]),
         disable_web_page_preview=True,
     )
-      
      
 @StreamBot.on_message(filters.command('help'))
 async def command(b, m:Message):
@@ -41,6 +40,68 @@ Here is the list of my commands, please read carefully everything. if anything h
             InlineKeyboardButton('🔙 Back', callback_data='home'),
             InlineKeyboardButton('📣 Channel', url='https://t.me/hagadmansa')
         ]]
+    ),
+        disable_web_page_preview=True,
+    )
+    
+@StreamBot.on_message(filters.command('howtouseme'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>ℹ️ Help</b> > How To Use Me
+        
+My name is Hagadmansa Mega Bot, I am a member of Hagadmansa family. I can provide you direct download link of any telegram file/media. If you send me any file/media I will give an external download link, you can use that link to download any file outside telegram. My link is supported in any browser.
+
+• Send me any file/media from Telegram.
+• I Will provide an external download link for you.
+• All links will be permanent and have the fastest speed.
+
+<b>🔞 Warning:</b>
+
+• 18+ content and pornography are strictly prohibited. Don't send me any pornographic/violent videos. You will get an instant ban if we see any kind of content like this.""",
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+    ),
+        disable_web_page_preview=True,
+    )
+    
+@StreamBot.on_message(filters.command('instructions'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>ℹ️ Help</b> > Instructions
+        
+1. Don't send photos to the bot, send them as a file.
+2. Don't send multiple files at a time, send them one by one.
+
+<b>🔞 Warning:</b>
+
+• 18+ content and pornography are strictly prohibited. Don't send me any pornographic/violent videos. You will get an instant ban if we see any kind of content like this.""",
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
+    ),
+        disable_web_page_preview=True,
+    )
+
+@StreamBot.on_message(filters.command('instructions'))
+async def command(b, m:Message):
+    await m.reply_text(
+        text="""<b>ℹ️ Help</b> > Tutorials
+        
+All tutorials related to Bots, Website, Movies and etc, will be updated here. Till then you can visit my movie website <b>www.hagadmansa.com</b> to watch movies. Don't forget to subscribe my updates channel <b>@hagadmansa</b>.
+
+<b>🔞 Warning:</b>
+
+• 18+ content and pornography are strictly prohibited. Don't send me any pornographic/violent videos. You will get an instant ban if we see any kind of content like this.""",
+        reply_markup=InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔙 Back', callback_data='help'),
+            InlineKeyboardButton('🏠 Home', callback_data='home')
+            ]]
     ),
         disable_web_page_preview=True,
     )

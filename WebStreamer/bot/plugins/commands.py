ba548416f2d8b6ec15c32874f73c3d701f7c7d45
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 23:20:37 +0530

    Update commands.py

--- WebStreamer/bot/plugins/commands.py
+++ WebStreamer/bot/plugins/commands.py
@@ -87,7 +87,7 @@ async def command(b, m:Message):
         disable_web_page_preview=True,
     )
 
-@StreamBot.on_message(filters.command('instructions'))
+@StreamBot.on_message(filters.command('tutorials'))
 async def command(b, m:Message):
     await m.reply_text(
         text="""<b>ℹ️ Help</b> > Tutorials

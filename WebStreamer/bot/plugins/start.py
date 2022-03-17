6612e92ea0f5c0f7c31db8a83a4c1bbfe456c1d9
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 12:44:30 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -10,13 +10,4 @@ async def start(_, m: Message):
     await m.reply(
         f'follow me on telegram <a href="https://hagadmansa.com/telegram/">here</a>.'
     )
-    
-@Client.on_message(filters.command("start") 
-async def start(client, message):
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-
-START_TEXT = """
-👋 Hello {},
-🤖 My Name is Hagadmansa Mega Bot, I can stream Telegram Files over HTTP.
-🧐 Don't know how to do? No worries, just press the help button.
-👨‍💻 My Creator is <a href=https://t.me/hagadmansa>Hagadmansa</a>."""
+   

9f05d5b4c45fd8917e8e56bb5a46271e5886549b
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 18:41:27 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -215,16 +215,9 @@ async def cb_data(bot, update):
             disable_web_page_preview=True,
             reply_markup=INSTRUCTIONS_BUTTONS
         )
-    elif update.data == "tutorials":
-        await update.message.edit_text(
-            text=TUTORIALS_TEXT,
-            disable_web_page_preview=True,
-            reply_markup=TUTORIALS_BUTTONS
-        )
-     elif update.data == "about":
+    elif update.data == "about":
         await update.message.edit_text(
             text=ABOUT_TEXT,
             disable_web_page_preview=True,
             reply_markup=ABOUT_BUTTONS
-        )     
-    
+        )

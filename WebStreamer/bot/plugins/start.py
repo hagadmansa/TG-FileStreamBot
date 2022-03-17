2774b4af2be2e9de825a8e38588027f6a2b04f58
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-17 21:17:39 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -24,25 +24,6 @@ async def command(b, m:Message):
         disable_web_page_preview=True,
     )
       
-@StreamBot.on_message(filters.command('help'))
-async def command(b, m:Message):
-    await m.reply_text(
-        text="""<b>ℹ️ HELP</b>
-        
-Here is the list of my commands, please read carefully everything. if anything happened to you then we are not responsible."""
-        reply_markup=InlineKeyboardMarkup
-      ([[
-            InlineKeyboardButton('❓ How to use me', callback_data='howtouseme')
-            ],[
-            InlineKeyboardButton('⚙️ Instructions', callback_data='instructions'),
-            InlineKeyboardButton('🕹 Tutorials', callback_data='tutorials'),
-            ],[
-            InlineKeyboardButton('🔙 Back', callback_data='home'),
-            InlineKeyboardButton('📣 Channel', url='https://t.me/hagadmansa')
-        ]]),
-        disable_web_page_preview=True,
-    )
-    
 START_TEXT = """
 👋 Hello {},
 

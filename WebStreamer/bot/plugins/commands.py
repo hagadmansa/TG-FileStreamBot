43491771039c7a57abcc239f321d833aab84a37a
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-18 18:11:13 +0530

    Update commands.py

--- WebStreamer/bot/plugins/commands.py
+++ WebStreamer/bot/plugins/commands.py
@@ -5,6 +5,8 @@ from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+START_TEXT=...
+
 @StreamBot.on_message(filters.command('start'))
 async def command(b, m:Message):
     await m.reply_text(

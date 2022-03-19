a7d2c47dd7f81976af1bcfe0d73142f6300a47a0
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-20 01:46:52 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -30,7 +30,7 @@ async def media_receive_handler(_, m: Message):
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     await m.reply_text(
         text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
-            stream_link, short_link ),
+            stream_link, short_link),
         reply_markup=InlineKeyboardMarkup(
             [[
               InlineKeyboardButton(text='Full Link', url='stream_link'),

cfd45335f943fa2935be3000b9dbd07029add515
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-20 10:21:38 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -222,13 +222,13 @@ async def cb_data(bot, update):
 @StreamBot.on_message(filters.command('start'))
 async def command(b, m:Message):
     await m.reply_text(
-        text="""👋 <b>Hello</b> {m.from_user.mention(style="md")},
+        text="""👋 <b>Hello</b> {},
 
 🤖 My Name is Hagadmansa Mega Bot, I can stream Telegram Files over HTTP.
 
 🧐 Don't know how to do? No worries, just press the help button.
 
-👨‍💻 My Creator is <a href='https://t.me/hagadmansa>Hagadmansa'</a>.""",
+👨‍💻 My Creator is <a href='https://t.me/hagadmansa>Hagadmansa'</a>.""".format{m.from_user.mention(style="md")},
         reply_markup=InlineKeyboardMarkup
         ([[
             InlineKeyboardButton('🌐 Website', url='https://hagadmansa.com'),

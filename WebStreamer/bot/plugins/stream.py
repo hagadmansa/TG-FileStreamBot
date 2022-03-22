c9a21e7d581790c9fd311fce16ae9343605b5f36
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-22 19:46:26 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -30,7 +30,7 @@ async def media_receive_handler(_, m: Message):
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     
     await log_msg.reply_text(
-            text=f"Here is the link genreated by {m.from_user.mention(style="md")}",
+            text=f"Here is the link genreated by {m.from_user.mention(style='md')}",
             quote=True,
             parse_mode="HTML"
         )

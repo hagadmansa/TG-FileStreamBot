5024b6b833c725f3945f6ea2ee03cb41b645cc61
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-03-22 15:51:35 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -45,9 +45,7 @@ async def media_receive_handler(_, m: Message):
 @StreamBot.on_message(filters.channel & (filters.document | filters.video) & ~filters.edited, group=-1)
 async def channel_receive_handler(bot, broadcast):
    await bot.edit_message_reply_markup(
-            chat_id=broadcast.chat.id,
-            message_id=broadcast.message_id,
-            log_msg = await bot.forward(chat_id=Var.BIN_CHANNEL),
+            log_msg = await broadcast.forward(chat_id=Var.BIN_CHANNEL),
             reply_markup=InlineKeyboardMarkup(
         [[
             InlineKeyboardButton('📥 Full link', url="hgffyy.com"),

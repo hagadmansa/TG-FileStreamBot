d361837378c3b67e5a67ceac44a094bdce19f735
hagadmansa <95907824+hagadmansa@users.noreply.github.com>
2022-04-02 01:45:50 +0530

    Update exceptions.py

--- WebStreamer/server/exceptions.py
+++ WebStreamer/server/exceptions.py
@@ -1,6 +1,6 @@
 
 class InvalidHash(Exception):
-    message = "This file has been deleted due to some important reasons, if you want to download this file or you have generated the link of this file or you are the owner of this file please contact us on telegram at @hagadmansachat. (Note: Do not forget to copy the url of this file as a proof.)"
+    message = "If you are redirected here from www.hagadmansa.com in case of finding a movie, then please wait some time we'll try to recover this movie as soon as possible. Till then you can message us on Telegram at @hagadmansachat we'll provide an alternative movie file for you. /n/nThis file has been deleted due to some important reasons, if you want to download this file or you have generated the link of this file or you are the owner of this file please contact us on telegram at @hagadmansachat. /n/n(Note: Do not forget to copy the url of this file as a proof.)"
 
 class FIleNotFound(Exception):
     message = "This file has been deleted due to some important reasons, if you want to download this file or you have generated the link of this file or you are the owner of this file please contact us on telegram at @hagadmansachat. (Note: Do not forget to copy the url of this file as a proof.)"
